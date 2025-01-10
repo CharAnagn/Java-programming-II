c09845f2ebad45cf1087ddca07c14891f921d42e
Babis Anagnostou <charalampos@smallgiants.nl>
2025-01-10 20:21:58 +0100

    done

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,27 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int count = 0;
+
+        while (true) {
+          Integer userInput = Integer.valueOf(scanner.nextLine());
+
+          if (userInput == 0) {
+              break;
+          }
+
+          if (userInput > 0) {
+              sum += userInput;
+              count++;
+          }
+        }
+
+        if (sum > 0) {
+            System.out.println(1.0 * sum / count);
+        } else {
+            System.out.println("Cannot calculate the average");
+        }
     }
+
 }
