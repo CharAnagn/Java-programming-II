093f5cb091789aecb5df3d41c2f915e18300fb74
Babis Anagnostou <charalampos@smallgiants.nl>
2025-01-10 20:16:18 +0100

    done

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -6,5 +6,17 @@ public class Cubes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+          String userInput = scanner.nextLine();
+
+          if (userInput.equals("end")) {
+              break;
+          }
+
+          Integer number = Integer.valueOf(userInput);
+
+          System.out.println(number * number * number);
+          
+        }
     }
 }
