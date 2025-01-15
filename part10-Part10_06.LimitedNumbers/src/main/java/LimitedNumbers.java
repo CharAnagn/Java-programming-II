ed8433f274f89194882a709164dcb158d01aa938
Babis Anagnostou <charalampos@smallgiants.nl>
2025-01-15 16:58:57 +0100

    done

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,21 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> inputs = new ArrayList<>();
+
+        while (true) {
+            Integer userInput = Integer.valueOf(scanner.nextLine());
+
+            if (userInput < 0) {
+                break;
+            }
+
+
+            inputs.add(userInput);
+        }
+
+
+        inputs.stream().filter(num -> num > 0 && num <=5).forEach(num -> System.out.println(num));
 
     }
 }
