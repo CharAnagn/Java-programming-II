05a23acc98e1ddba3a134e91f354e7d81b39af1a
Babis Anagnostou <charalampos@smallgiants.nl>
2025-01-15 16:56:14 +0100

    done

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -6,6 +6,19 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> inputs = new ArrayList<>();
+
+        while (true) {
+          String userInput = scanner.nextLine();
+
+          if (userInput.equals("")) {
+              break;
+          }
+
+          inputs.add(userInput);
+        }
+
+        inputs.stream().forEach(input -> System.out.println(input));
 
     }
 }
