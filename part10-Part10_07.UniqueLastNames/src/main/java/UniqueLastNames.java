cd9d820427c9ce60c74da307bc4458f56a0f1aac
Babis Anagnostou <charalampos@smallgiants.nl>
2025-01-15 17:06:43 +0100

    done

--- part10-Part10_07.UniqueLastNames/src/main/java/UniqueLastNames.java
+++ part10-Part10_07.UniqueLastNames/src/main/java/UniqueLastNames.java
@@ -31,6 +31,10 @@ public class UniqueLastNames {
             System.out.println("");
         }
 
+        System.out.println("Unique last names in alphabetical order:");
+
+        persons.stream().map(Person::getLastName).distinct().sorted().forEach(System.out::println);
+
         // Implement the printing of the unique last names in alphabetical order here:
     }
 }
