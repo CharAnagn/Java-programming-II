fcfe488166379ac6a7b7fb635460b76043c04a24
Babis Anagnostou <charalampos@smallgiants.nl>
2025-01-20 18:55:31 +0100

    done

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,17 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many random numbers should be printed?");
+        Integer userInput = Integer.valueOf(scanner.nextLine());
+
+        Random ladyLuck = new Random(); // create Random object ladyLuck
+
+        for (int i = 0; i < userInput; i++) {
+            // Draw and print a random number
+            int randomNumber = ladyLuck.nextInt(11);
+            System.out.println(randomNumber);
+        }
     }
 
 }
